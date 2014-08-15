aa114a748bab89fe77ef99211c18b7cc31f50dce
Sylvain Niles <sylvain.niles@gmail.com>
2014-08-15 12:08:52 -0700

    adjust radius by mercator distortion

--- sunspot/lib/sunspot/dsl/restriction_with_near.rb
+++ sunspot/lib/sunspot/dsl/restriction_with_near.rb
@@ -143,9 +143,10 @@ module Sunspot
       end
 
       def in_rpt_radius(lat, lon, radius, options = {})
+        adjusted_radius = (radius*1000.0 * (1 / Math.cos(lat / 180.0 * Math::PI)))
         factory = RGeo::Geographic.simple_mercator_factory(buffer_resolution: 4)
         projected_point = factory.project(factory.point(lon, lat))
-        @query.add_geo(Sunspot::Query::Geofilt.new(@field, projected_point.y, projected_point.x, radius, options))
+        @query.add_geo(Sunspot::Query::Geofilt.new(@field, projected_point.y, projected_point.x, adjusted_radius, options))
       end
 
       #
