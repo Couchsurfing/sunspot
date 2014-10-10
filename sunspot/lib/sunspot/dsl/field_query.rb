5d16ff5268d4478fa2cac59eec7f735cf210bf89
Sylvain Niles <sylvain.niles@gmail.com>
2014-10-10 16:27:59 -0700

    make sum recip

--- sunspot/lib/sunspot/dsl/field_query.rb
+++ sunspot/lib/sunspot/dsl/field_query.rb
@@ -85,7 +85,7 @@ module Sunspot
         def obj.to_params
           {
             sfield: field.indexed_name,
-            boost: "recip(sum(geodist(#{lat},#{lon}),ms(last_couch_visit_i, NOW)),#{denominator},1000,1000)",
+            boost: "recip(sum(geodist(#{lat},#{lon}),recip(ms(last_couch_visit_i, NOW),1,1,1)),#{denominator},1000,1000)",
             defType: "edismax" # this query format is specific to edismax
           }
         end
