db61a66eebcf4795251990d2c5a8303a0920a888
Sylvain Niles <sylvain.niles@gmail.com>
2014-11-03 20:58:47 -0800

    balance geo

--- sunspot/lib/sunspot/dsl/field_query.rb
+++ sunspot/lib/sunspot/dsl/field_query.rb
@@ -85,7 +85,7 @@ module Sunspot
         def obj.to_params
           {
             sfield: field.indexed_name,
-            boost: "sum(scale(recip(geodist(#{lat},#{lon}),#{denominator},1000,1000),1,120),sum(scale(host_score_i,1,80),scale(rord(last_couch_visit_i),1,5)))",
+            boost: "sum(scale(recip(geodist(#{lat},#{lon}),#{denominator},1000,1000),1,160),sum(scale(host_score_i,1,80),scale(rord(last_couch_visit_i),1,10)))",
             defType: "edismax" # this query format is specific to edismax
           }
         end
