3b3cd781b437e84589e3e4837d36bbd515415402
Sylvain Niles <sylvain.niles@gmail.com>
2014-11-03 16:02:53 -0800

    rounds geo up a bit

--- sunspot/lib/sunspot/dsl/field_query.rb
+++ sunspot/lib/sunspot/dsl/field_query.rb
@@ -85,7 +85,7 @@ module Sunspot
         def obj.to_params
           {
             sfield: field.indexed_name,
-            boost: "sum(scale(recip(geodist(#{lat},#{lon}),#{denominator},1000,1000),1,200),scale(host_score_i,1,20))",
+            boost: "sum(scale(recip(max(geodist(#{lat},#{lon}),400),#{denominator},1000,1000),1,120),scale(host_score_i,1,20))",
             defType: "edismax" # this query format is specific to edismax
           }
         end
